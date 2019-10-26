788efacb5633b305ce4bcc2ddee3d612e6ac0891
SynCoder <sujaykrshn@gmail.com>
2019-10-26 12:40:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/loginpage/ui/login/DoctororPatient.java
@@ -0,0 +1,16 @@
+package com.example.loginpage.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.loginpage.R;
+
+public class DoctororPatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctoror_patient);
+    }
+}
